8021680f351aeb6684cf21c4d85b41d043adad1e
adninandintian <andin.adnintiann@gmail.com>
2018-09-13 07:49:49 +0700

    first commit

---
+++ Judul.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+class Judul extends React.Component{
+	render(){
+		return(
+			
+			<Text style={salon.judul}
+			>BIODATA</Text>
+			)
+	}
+}
+
+const salon = {
+	judul:{
+		color : '#DA4D72',
+		fontSize : 50,
+		
+	}
+}
+export default Judul;
